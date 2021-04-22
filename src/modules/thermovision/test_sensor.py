9f5307d3a91cbcd5012d47eb5b7fa49714b7dbde
DawidCwiek <cwiekdawid1@gmail.com>
2021-04-22 21:20:52 +0200

    review -> add some comments and doc

---
+++ src/modules/thermovision/test_sensor.py
@@ -0,0 +1,12 @@
+from thermovision_sensor import ThermovisionSensor
+
+"""It allows you to check the correctness of the configuration
+and connection of the sensor.
+
+The function returns 10 average temperature values for an object
+with a temperature close to human temperature
+"""
+def main():
+    thermal_sensor = ThermovisionSensor()
+    for i in range(10):
+        print(thermal_sensor.get_temperature())
